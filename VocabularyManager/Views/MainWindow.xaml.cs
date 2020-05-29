5db9e15c12b1a2d8140c78ec9975b0dab4a44aa6
Anastasiya Kryven <kriven999@gmail.com>
2020-05-29 16:42:10 +0300

    Code cleaning

--- VocabularyManager/Views/MainWindow.xaml.cs
+++ VocabularyManager/Views/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using VocabularyManager.Models;
-using VocabularyManager.Services;
 using VocabularyManager.ViewModel;
 using VocabularyManagerService.Services;
 
