3de6b2fd1488fe376f1a4c94af5e065c5a45a8fa
Анастасия Кривень <kriven999@gmail.com>
2020-06-02 15:18:01 +0300

    Rename projects

---
+++ VolumeManager/Models/VolumeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeManager.Models
+{
+    public class VolumeModel
+    {
+        public float SoundValue { get; set; }
+    }
+}
